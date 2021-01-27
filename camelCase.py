d312dfaf69b878c480813c8e2ef277e873426b21
chris <cgisala@msn.com>
2021-01-27 02:47:42 -0600

    remove old code and added a comment

--- camelCase.py
+++ camelCase.py
@@ -1,40 +1,5 @@
 # This program turns a sentence into camel case.
 
-# sentence = input("Type a sentence: \n")
-# sentence = "fOnt proCESSOR and ParsER"
-# print(f"string: {sentence}")
-# print()
-
-# lst = [] # initializes a list
-# lst.append(sentence) # add the string in a list
-
-
-# # This function takes a string and split it into different elements
-# def convert(lst):
-#     return(lst[0].split())
-
-# # converts string into a list
-# stringToList = convert(lst)
-# print(f'list: {stringToList}')
-# print()
-
-
-# stringToList2 = [] # initializing the second list
-# for num in stringToList:
-#         stringToList2.append(num.title())
-
-# print(f'string to list: {stringToList2}')
-# print()
-
-# stringToList2[0] = stringToList2[0].lower()
-
-# stringToList = stringToList2
-
-# # Converts list to string
-# listToString = ''.join(stringToList)
-
-# Camel Case
-print(f'camel case string: {listToString}')
 def camelcase(sentence):
     """ Convert sentence to camelCase, for example, "Display all books" 
     is converted to "displayAllBooks" """
@@ -45,14 +10,14 @@ def camelcase(sentence):
     #So this still works on empty strings, strings of length 1
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
-def display_banner():
+def display_banner(): # Creates a banner
     """Display program name in banner"""
     msg = 'AWSOME camelCaseGenerator PROGRAM'
     stars = '*' * len(msg)
     print(f'\n {stars} \n {msg} \n {stars}\n')
 
 def main():
-    display_banner():
+    display_banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
